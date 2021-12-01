143346ca5a5e9c6d88052d771929cb80ee1d3540
Dmitrii Onegin <dimimimo@mail.ru>
2021-12-01 16:37:30 +0300

    Initial Commit

---
+++ GitDemo/GitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoApp.swift
+//  GitDemo
+//
+//  Created by Dmitrii Onegin on 01.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
